e55b2ddf9208fb8f542af397a8044ff01499a72a
selzehairy <selzehairy@gmail.com>
2023-07-24 05:03:13 -0700

    go state

--- models/state.py
+++ models/state.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
-"""Defines the State class."""
-import models
+"""
+State Class:
+    Inherits from BaseModel and Base
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
+from models import storage
 from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
-from models.city import City
-from sqlalchemy import Column
-from sqlalchemy import String
-from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """Represents a state for a MySQL database.
-
-    Inherits from SQLAlchemy Base and links to the MySQL table states.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store States.
-        name (sqlalchemy String): The name of the State.
-        cities (sqlalchemy relationship): The State-City relationship.
     """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
-
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    Class to handle state objecs
+    Cities and places inherit from this class
+    """
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="delete", backref="state")
+    else:
         @property
         def cities(self):
-            """Get a list of all related City objects."""
-            city_list = []
-            for city in list(models.storage.all(City).values()):
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+            stor = storage.all('City').values()
+            return ([a for a in stor if self.id == a.state_id])
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        super(State, self).__init__(*args, **kwargs)
